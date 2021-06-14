90df5c25c7d17423a109925bf224081b74b5c788
higashi takuma <recurrent.higashi@gmail.com>
2021-06-14 14:43:46 +0900

    Formに変えたやつ

---
+++ workspace/spring-team/src/main/java/com/example/demo/LoginForm.java
@@ -0,0 +1,27 @@
+package com.example.demo;
+
+public class LoginForm {
+	private String email;
+	private String password;
+	
+	public LoginForm() {
+		
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+}

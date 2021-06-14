1b77263dafd04534cafc235bf6b2b1a83928a265
higashi takuma <recurrent.higashi@gmail.com>
2021-06-14 17:53:42 +0900

    検索フォーム仮

--- workspace/spring-team/src/main/java/com/example/demo/FrameController.java
+++ workspace/spring-team/src/main/java/com/example/demo/FrameController.java
@@ -1,5 +1,10 @@
 package com.example.demo;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,9 +15,13 @@ import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class FrameController {
+	private static final String DATEFORMAT = "yyyy-MM-dd";
 	
 	@Autowired
 	HttpSession session;
+	
+	@Autowired
+	PrefecturesRepository prefecturesRepository;
 	
 	@RequestMapping("/")
 	public String login() {
@@ -29,6 +38,20 @@ public class FrameController {
 	
 	@RequestMapping("/search")
 	public ModelAndView search(ModelAndView mv) {
+		//本日の日付の取得
+		Date date = new Date();
+		SimpleDateFormat sdFormat = new SimpleDateFormat(DATEFORMAT);
+		String checkinDate = sdFormat.format(date);
+		mv.addObject("checkinDate", checkinDate);
+		//明日の日付の取得
+		Calendar calendar = Calendar.getInstance();
+		calendar.add(Calendar.DAY_OF_MONTH, 1);
+		date = calendar.getTime();
+		String checkoutDate = sdFormat.format(date);
+		mv.addObject("checkoutDate", checkoutDate);
+		//都道府県リストの取得
+		List<Prefectures> prefecturesList = prefecturesRepository.findAll();
+		mv.addObject("prefecturesList",prefecturesList);
 		mv.setViewName("search");
 		return mv;
 	}

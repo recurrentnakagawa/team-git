41facf1ba21ee36353da38ba45c6ce8ae0420d87
Yuto Nakagawa <recurrent.nakagawa@gmail.com>
2021-06-16 10:16:12 +0900

    controllミス

--- workspace/spring-team/src/main/java/com/example/demo/AccountFrame.java
+++ workspace/spring-team/src/main/java/com/example/demo/AccountFrame.java
@@ -1,13 +1,24 @@
 package com.example.demo;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
-
+@Controller
 public class AccountFrame {
 	
 	@RequestMapping("/userMenu")
 	public ModelAndView userMenu(ModelAndView mv) {
 		mv.setViewName("userMenu");
+		return mv;
+	}
+	@RequestMapping("/userRegistFrame")
+	public ModelAndView userRegistFrame(ModelAndView mv) {
+		mv.setViewName("userRegistFrame");
+		return mv;
+	}
+	@RequestMapping("/userLoginFrame")
+	public ModelAndView userLoginFrame(ModelAndView mv) {
+		mv.setViewName("userLoginFrame");
 		return mv;
 	}
 }
